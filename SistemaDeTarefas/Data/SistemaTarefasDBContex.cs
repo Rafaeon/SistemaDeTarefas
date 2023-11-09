25fe63e942d6dbd3b64b12b572e4d5054db11a14
Rafael <rafeltorress1@gmail.com>
2023-11-08 22:43:31 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaDeTarefas/Data/SistemaTarefasDBContex.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Data
+{
+    public class SistemaTarefasDBContex : DbContext
+    {
+
+        public SistemaTarefasDBContex(DbContextOptions<SistemaTarefasDBContex> opitions)
+            : base(opitions)
+        {
+        }
+
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+        public DbSet<TarefaModel> tarefa { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
